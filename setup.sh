d6b37237e82b03117c3f6685241d3e87e6de39b8
AbdelMonim DAKKON <71790032+AbdellM@users.noreply.github.com>
2022-03-10 14:21:14 +0100

    Update added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+eval "sudo apt-get update"
+
 echo "-> Pip installing..."
-eval "sudo apt install python3-pip"
+eval "apt install python3-pip -y"
 
 echo "-> Powerline installing..."
 eval "pip3 install --user powerline-status"
@@ -22,7 +24,7 @@ echo 'POWERLINE_BASH_CONTINUATION=1' >> ~/.bashrc
 echo 'POWERLINE_BASH_SELECT=1' >> ~/.bashrc
 echo 'source' ${output2} >> ~/.bashrc
 
-echo "mkdir -p ~/.config/powerline/colorschemes"
-echo "mkdir -p ~/.config/powerline/themes/shell"
-echo "cp colorschemes/default.json ~/.config/powerline/colorschemes/"
-echo "cp themes/default.json ~/.config/powerline/themes/shell/"
+eval "mkdir -p ~/.config/powerline/colorschemes"
+eval "mkdir -p ~/.config/powerline/themes/shell"
+eval "cp colorschemes/default.json ~/.config/powerline/colorschemes/"
+eval "cp themes/default.json ~/.config/powerline/themes/shell/"
